5baff758c44e394c50863e7bd9608c3f52138e37
abdiwak_bekele <abdiwakbek3226@gmail.com>
2025-01-20 20:00:09 +0300

    updated

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,11 @@ import './bootstrap';
 import { createApp } from 'vue';
 import app from './components/app.vue'
 import router from './components/router/index.js'
+import store from './store';
 
 
 
-createApp(app).use(router).mount("#app")
+createApp(app)
+    .use(router)
+    .use(store)
+    .mount("#app");
